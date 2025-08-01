a3e8ddabd456052e9b8c95b5d247c18bfece2cef
Yulia <yuliashaifele@gmail.com>
2025-08-01 16:43:03 +0400

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     locale: 'ru',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
 
   },
 
