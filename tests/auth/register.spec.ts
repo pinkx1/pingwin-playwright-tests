7fe5dc57f6236b323e7a27eeb138020b9745d1e3
Yulia <yuliashaifele@gmail.com>
2025-08-07 00:29:55 +0400

    test

--- tests/auth/register.spec.ts
+++ tests/auth/register.spec.ts
@@ -10,10 +10,13 @@ function generateAutotestEmail(): string {
 }
 
 function generateRandomPhone(): string {
+  const prefix = Math.floor(Math.random() * 900 + 100);
   const middle = Math.floor(Math.random() * 90 + 10);
   const last = Math.floor(Math.random() * 90 + 10);
-  return `(33)211-${middle}-${last}`;
+  return `(33)${prefix}-${middle}-${last}`;
 }
+
+
 
 const invalidEmail = 'notanemail';
 const sevenCharPassword = '1234567';
